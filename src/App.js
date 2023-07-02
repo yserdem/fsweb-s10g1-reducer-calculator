b4b34f4ff5434ca9bf2c0c7a37f118bdb7f6689c
Sinan <y.sinanerdem@gmail.com>
2023-07-02 11:49:28 +0300

    ADD_ONE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 import reducer, {initialState} from './reducers';
+import { ADD_ONE, addOne } from './actions';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -29,7 +30,9 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton 
+              onClick= {()=> dispatch({type: ADD_ONE})}
+              value={1} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
